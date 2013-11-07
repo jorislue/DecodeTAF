9d0bf2bb97beba0d6c19c2cb518c7318163029cc
jorislue <joris.lueckenga@gmail.com>
2013-11-07 13:18:13 +0100

    Initial Commit

--- DecodeTAF.cpp
+++ DecodeTAF.cpp
@@ -1,6 +1,7 @@
 #include "DecodeTAF.h"
 #include <iostream>
-
+#include "DecodeDLL.h"
+//http://msdn.microsoft.com/de-de/library/ms235636(v=vs.90).aspx
 using namespace std;
 
 DecodeTAF::DecodeTAF(void)
@@ -15,5 +16,6 @@ DecodeTAF::~DecodeTAF(void)
 
 void main(){
 	cout<<"DecodeTAF Console Application"<<endl;
+	cout<<returnDecodedTAF("bla");
 	system("pause");
 }
